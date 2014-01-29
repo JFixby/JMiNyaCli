4208820fb5a9314948aeb45a0e677aed71d10a2f
nyatla <nyatla39@gmail.com>
2014-01-29 23:27:14 +0900

    add licence header.

--- core/src/jp/nyatla/minya/worker/CpuMiningWorker.java
+++ core/src/jp/nyatla/minya/worker/CpuMiningWorker.java
@@ -1,3 +1,22 @@
+/********************************************************************************
+ * MiNya pjeject
+ * Copyright 2014 nyatla.jp
+ * https://github.com/nyatla/JMiNya
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ ********************************************************************************/
+
 package jp.nyatla.minya.worker;
 
 import java.security.GeneralSecurityException;
@@ -7,7 +26,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import jp.nyatla.minya.MiningWork;
-import jp.nyatla.minya.Minya;
 import jp.nyatla.minya.MinyaException;
 import jp.nyatla.minya.MinyaLog;
 import jp.nyatla.minya.hasher.Hasher;
